f49f72cd48855308a0d6cbd2de5cad7a47f139c5
Anchal singh <anchalsingh8790@gmail.com>
2026-08-09 20:07:17 +0530

    add some codes

---
+++ PYTHON PHASE 1/simpleinterest.py
@@ -0,0 +1,5 @@
+P=int(input("Enter the principal amount: "))
+R=float(input("Enter the rate of interest: "))
+T=int(input("Enter the time in years: "))
+SI=(P*R*T)/100
+print("The simple interest is:",SI)
